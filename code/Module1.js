563b23871da232ec07c58eac2e3b50f52063f0cb
Connor73322 <cjackson20006@aol.com>
2017-03-10 09:07:30 -0500

    Create Module1.js

---
+++ code/Module1.js
@@ -0,0 +1,10 @@
+# Connors Practice script
+
+#Type rect(); and add numbers to make a rectangle for example
+rect(100, 200, 79, 176);
+
+#To make a circle its ellipse();
+ellipse(150, 150, 200, 200);
+
+#To make a triangle type triangle();
+triangle(100, 100, 300, 100, 200, 300);
