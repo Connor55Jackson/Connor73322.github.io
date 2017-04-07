bba8d091ff945918c956d8d57e1fd292950e69be
Connor73322 <cjackson20006@aol.com>
2017-04-07 08:39:30 -0400

    Create Module4.js

---
+++ code/Module4.js
@@ -0,0 +1,9 @@
+//Equations using variables
+var x = 150
+var y = 150
+
+//adding them in the drawing
+ellipse(x, y, 300, 300); // the head
+
+ellipse(x - 50, y, 50, 50); // left eye
+ellipse(x + 100, y, 50, 50); // right eye
